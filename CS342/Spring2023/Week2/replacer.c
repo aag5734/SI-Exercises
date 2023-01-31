c87e174821f2455ee5aac3a609639f8d666a7f4d
aag5734 <abrahamgonzalez3764@gmail.com>
2023-01-31 14:06:01 -0500

    Added md for Week3A

--- CS342/Spring2023/Week2/replacer.c
+++ CS342/Spring2023/Week2/replacer.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-/// main does stuff
+#define BUFFERSIZE 256
+
+const char* search_and_replace(char line[], char wordToReplaceWith[], char word[])
+{
+  int currWordIndex = 0; // records our current index
+  char currWord[BUFFERSIZE] = ""; // holds each word in the line
+  char newLine[BUFFERSIZE] = ""; // the replacement line to print out
+  for(int i = 0; i < (int) strlen(line); i++)
+  {
+    char currChar = (char) line[i];
+    if(currChar == '.' || currChar == ' ' || currChar == ',' || currChar == '\n')
+    {
+      if(strcmp(word, currWord) == 0)
+      {
+        for(int f = currWordIndex; ) // tryna figure out how to replace without screwing up word order, using the current index and what have you imma figure this out laters bye
+      }
+      else
+      {
+        
+      }
+    }
+    currWordIndex++;
+  }
+  
+  return ;
+}
+
+/// main complete
 /// tru
 /// @param argv[1] is the word/phrase to be replaced
 /// @param argv[2] is the word/phrase we're going to replace it with
@@ -10,23 +39,30 @@
 int main(int argc, char* argv[]) {
 	char * placeholder [];
     if (argc < 2) {
-    	printf("lazy bum didn't put enough command line arguments");
-        printf("Usage: word word filename");
+    	printf("lazy bum didn't put enough command line arguments\n");
+        printf("Usage: word word filename\n");
     	return EXIT_FAILURE;
     }
-    char toReplace[] = argv[1];
-    char word[] = argv[2];
-    
-    FILE *fptr;
-    fptr = fopen("path", "w+");
-    if (fptr == NULL) {
-    	printf("File does not exist");
-        return EXIT_FAILURE;
-    }
   
+  	///The word to be replaced
+    char toReplace[] = argv[1];
+		///The word to replace it with
+    char word[] = argv[2];
+  
+  	///The storage for the current line we're reading
+  	char line[]
+  
+    ///Main loop
   	for(;;) {
-      fscanf(fptr, %s, placeholder);
-      if (placeholder)
+      ///Checks for EOF
+      if (fgets(line, BUFFERSIZE, stdin) == NULL) {
+        if(feof(stdin)) {
+          break;
+        }
+      }
+      
+      ///Prints out modified solution.
+      printf("%s\n", search_and_replace(line, toReplace, word));
     }
   	
 	return EXIT_SUCCESS;
---
+++ CS342/Spring2023/Week3/3A/Scope&Stack.md
@@ -0,0 +1,43 @@
+# Scope and the Stack
+
+This session will be pretty light in terms of new material, drawing mostly from the last session's programming
+assignment as the bulk of the content.
+
+## MAIN CONCEPTS
+
+- Stack
+    - Where local memory is stored in the OS
+- Scope
+    - Defines what and how our functions/variables are called
+        - static
+            - private
+        - no keyword
+            - public
+
+## SESSION ACTIVITIES   
+
+### Wordle
+
+Spend the first 5-ish minutes playing wordle. If people have already played today’s wordle, just have that 
+person write out the letters on the board (and possibly give hints)
+
+
+### Braindump
+
+Use the next five minutes to have students put what they are confident in and what they are unsure about 
+in C on the whiteboard. Leave this on the board until we are near the end of the session.
+
+At the end of the session, go over some of the concepts the students wrote down and have them explain 
+to each other what they wrote down. 
+
+
+### Replacer Program Cont.
+
+We started on a program that read input from a file last session. Spend about 20 minutes or so working 
+some more on this. If we don’t finish in time, I’ll show the solution next session. 
+
+
+### Quick Review of Scope and Stacks
+
+As the title suggests, we’re going to quickly go over the stack and scope in C. Should take approximately 
+5 minutes.
