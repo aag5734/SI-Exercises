ff8c0d2b4e9b0012e6b7d6d8e78e4a0a161da074
aag5734 <abrahamgonzalez3764@gmail.com>
2023-02-02 15:01:09 -0500

    IDK

--- CS342/Spring2023/Week2/2B/replacer.c
+++ CS342/Spring2023/Week2/2B/replacer.c
@@ -1,11 +1,30 @@
+/**
+ * FILE: replacer.c
+ * AUTHOR: Abraham Gonzalez
+ * DATE: 02/02/2023
+ * DESC: Given some file, a word to be searched for, and a new word, we want to read the file's
+ * contents and output it to stdout while replacing one word with a new word. Ex; ./replacer words.txt John Smith
+ * replaces "John" with "Smith"
+*/
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 int main(int argc, char * argv[]) {
-    char s[100];
+    FILE * file = fopen(argv[1], "r+");
+    char * line;
+    char * curr;
 
-    for (int i = 0; i < argc; i++) {
-        printf("%s\n", argv[i]);
+    while (fgets(line, sizeof(line), file)) {
+        curr = strtok(line, " ");
+
+        while (curr != NULL) {
+            if (strcmp(curr, argv[2])) {
+                printf("%s ", argv[3]);
+            } else {
+                printf("%s ", curr);
+            }
+            curr = strtok(NULL, " ");
+        }
     }
 }
