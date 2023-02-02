3872209ca1c4db0e83a85c2a35a6f1df299fba23
aag5734 <abrahamgonzalez3764@gmail.com>
2023-02-02 14:11:02 -0500

    Updated solution

--- CS342/Spring2023/Week3/3B/students.c
+++ CS342/Spring2023/Week3/3B/students.c
@@ -1,9 +1,18 @@
+/**
+ * FILE: students.c
+ * AUTHOR: Abraham Gonzalez
+ * DATE: 02/02/2023
+ * DESC: Simple program that creates an array (length 5) of Student structs by reading
+ *      a file with a student name, their grade, and major on each line
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define n 60
 #define m 120
+#define k 5
 
 struct student {
     char name[n];
@@ -13,13 +22,14 @@ struct student {
 
 int main(int argc, char * argv[]) {
     FILE * file = fopen(argv[1], "r");
-    struct student students[n];
+    struct student students[k];
 
     char curr[m];
 
     char * name;
     char * grade;
     char * major;
+    int i = 0;
 
     while (fgets(curr, sizeof(curr), file)) {
         name = strtok(curr, " ");
@@ -27,9 +37,10 @@ int main(int argc, char * argv[]) {
         major = strtok(NULL, " ");
 
         struct student s = {name, atof((const) grade), major};
+        students[i] = s;
 
         printf("%s\n", name);
         printf("%s\n", grade);
         printf("%s\n", major);
-    } 
+    }
 }
