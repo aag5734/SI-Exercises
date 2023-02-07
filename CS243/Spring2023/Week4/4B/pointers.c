7387b3b6b7b2283ec7dbfb324699527f6ba8c224
aag5734 <abrahamgonzalez3764@gmail.com>
2023-02-07 15:08:40 -0500

    Mmmmm.....pointers....

---
+++ CS243/Spring2023/Week4/4B/pointers.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+    int * x;
+    int * y;
+    int val1 = 5;
+    int data[3] = {1, 2, 3};
+
+    /**
+     * COMPARISON 1
+    */
+    x = &val1;
+    y = x;
+    printf("%d\n", (x == y));
+
+    /**
+     * COMPARISON 2
+    */
+   y = 5;
+   printf("%d\n", (x == y));
+
+   /**
+    * COMPARISON 3
+   */
+  x = data;
+  y = data + 1;
+  printf("%d\n", ((x++) == y));
+}
