500da386b8f31ae862a58db8228f2a23bf6da68a
aag5734 <abrahamgonzalez3764@gmail.com>
2023-02-07 15:15:57 -0500

    Added a comparison

--- CS243/Spring2023/Week4/4B/pointers.c
+++ CS243/Spring2023/Week4/4B/pointers.c
@@ -13,17 +13,23 @@ int main(int argc, char * argv[]) {
     x = &val1;
     y = x;
     printf("%d\n", (x == y));
-
+    
     /**
      * COMPARISON 2
     */
-   y = 5;
-   printf("%d\n", (x == y));
+    y = 5;
+    printf("%d\n", (x == y));
 
    /**
     * COMPARISON 3
    */
-  x = data;
-  y = data + 1;
-  printf("%d\n", ((x++) == y));
+   x = data;
+   y = data + 1;
+   printf("%d\n", ((x++) == y));
+ 
+  /**
+   * COMPARISON 4
+  */
+  x = data + 2;
+  printf("%d\n", ((x) == &data[2]));
 }
