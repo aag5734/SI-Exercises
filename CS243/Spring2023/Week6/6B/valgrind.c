9615aa3f59414a5ab0676ce52fb43a7bd10468dc
aag5734 <abrahamgonzalez3764@gmail.com>
2023-02-23 13:49:42 -0500

    forgot this lol

---
+++ CS243/Spring2023/Week6/6B/valgrind.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char * argv[]) {
+    /**
+    Problem 1
+    */
+    float f = 0;
+
+    if (f == 12.4) {
+        printf("That's 12.4 alright.\n");
+    }
+
+    /**
+    Problem 2
+    */
+    int * ptr;
+
+    ptr = (int *) malloc(10 * sizeof(int));
+
+    ptr[0] = 1;
+
+    free(ptr);
+
+    /**
+    Problem 3
+    */
+    char ** str = (char **) malloc(10 * sizeof(char *));
+
+    for (int i = 0; i < 10; i++) {
+        str[i] = (char *) malloc(5 * sizeof(char));
+    }
+    for (int i = 0; i < 10; i++) { 
+        free(str[i]);
+    }
+    free(str);
+}
